22ad949c7dc93bf7331296bc2ad5136f62df5573
banfreddy <banzafreddy78@gmail.com>
2022-07-10 19:59:24 +0200

    prmier commit pour tous les fichier

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Home from './composants/Home';
+import Navbar from './composants/NavBar';
+
+
+
+function App() {
+    return ( 
+        <div className = "App" >
+        <Navbar/>
+        <div className="contenu">
+            <Home/>
+        </div>
+
+        </div>
+    );
+}
+
+export default App;
---
+++ src/composants/NavBar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+        <nav className = "navbar">
+            <div>
+                <a href = "" className = "logo" > Alba Blog </a> 
+            </div> 
+            <ul className = "liens">
+                <li>
+                    <a href = ""className = "lien" > Accueil </a> 
+                </li> 
+                <li>
+                    <a href = "" className = "lien" > Créer Articles </a> 
+                </li> 
+            </ul> 
+        </nav>
+    );
+}
+
+export default Navbar;
